7785bde29bd15cf0b3aee9d103fd05d2d656cb01
yujunhan111 <103967653+yujunhan111@users.noreply.github.com>
2025-03-30 21:39:55 +0800

    Delete our directory

--- our/death_weight.py
+++
@@ -1,6 +0,0 @@
-import torch
-death_weights = {
-            'in_hosp_die': torch.tensor([45.0]).cuda(),
-            'icu_die': torch.tensor([2.0]).cuda(),
-            'icu_24hour_die': torch.tensor([2.0]).cuda()
-        }
